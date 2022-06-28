38c70c2238a5b4a170071dda832fbec2fd8d1b54
BenhurUFSM <84202210+BenhurUFSM@users.noreply.github.com>
2022-06-28 19:56:41 -0300

    Add files via upload

--- Complementos/exemplo.c
+++ Complementos/exemplo.c
@@ -21,6 +21,25 @@ int main(void)
 
   // laço principal
   while (true) {
+    // desenha um quadrado no contorno da janela
+    tela_retangulo(30, 30, 470, 470, 5, amarelo, transparente);
+
+    // faz uma linha se o botão do mouse tiver apertado
+    if (tela_rato_apertado()) {
+      tela_linha(30, 30, tela_rato_x(), tela_rato_y(), 2, branco);
+    }
+
+    // desenha um círculo na posição do mouse
+    tela_circulo(tela_rato_x(), tela_rato_y(), 10, 2, vermelho, branco);
+
+    // escreve o valor do relógio no canto da tela
+    char txt[10];
+    sprintf(txt, "%.1f", relogio());
+    tela_texto_esq(450, 445, 20, azul, txt);
+
+    // faz aparecer na tela o que foi desenhado
+    tela_atualiza();
+
     // sai do laço quando algo for digitado no teclado
     if (tela_tecla() != '\0') {
       break;
